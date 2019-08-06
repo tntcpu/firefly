46d30a99a8745ec8ddd08bc09544b52814d03b3e
zhangzhentao <tntcpu@126.com>
2019-08-06 17:33:19 +0800

    java concurrency add!

--- jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/example/ThreadVarations.java
+++ jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/example/ThreadVarations.java
@@ -201,5 +201,6 @@ public class ThreadVarations {
 		new InnerThread2("InnerThread2");
 		new InnerRunnable1("InnerRunnable1");
 		new InnerRunnable2("InnerRunnable2");
+		new ThreadMethod("ThreadMethod").runTask();
 	}
 }
