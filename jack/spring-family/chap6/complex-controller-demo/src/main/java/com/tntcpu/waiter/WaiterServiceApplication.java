bd76511253930750ff1a92e24000db45a8ba7bfb
tntcpu <gpscpu@gmail.com>
2019-09-02 07:57:17 +0800

    complex-controller-demo add first!

---
+++ jack/spring-family/chap6/complex-controller-demo/src/main/java/com/tntcpu/waiter/WaiterServiceApplication.java
@@ -0,0 +1,4 @@
+package com.tntcpu.waiter;
+
+public class WaiterServiceApplication {
+}
---
+++ jack/spring-family/chap6/complex-controller-demo/src/main/java/com/tntcpu/waiter/service/CoffeeOrderService.java
@@ -0,0 +1,13 @@
+package com.tntcpu.waiter.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@Slf4j
+public class CoffeeOrderService {
+
+}
