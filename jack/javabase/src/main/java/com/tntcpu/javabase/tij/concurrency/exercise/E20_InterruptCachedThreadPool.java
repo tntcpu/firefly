8f65d911af5d21170b855f828e037164c92e799f
zhangzhentao <tntcpu@126.com>
2019-09-20 10:21:48 +0800

    java concurrency add!

--- jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/exercise/E20_InterruptCachedThreadPool.java
+++ jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/exercise/E20_InterruptCachedThreadPool.java
@@ -1,7 +1,5 @@
 package com.tntcpu.javabase.tij.concurrency.exercise;
 
-import com.tntcpu.javabase.tij.concurrency.example.LiftOff;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,21 +14,26 @@ class LiftOff2 implements Runnable {
 	private static int taskCount;
 	private final int id = taskCount++;
 
-	public LiftOff2(){}
-	public LiftOff2(int countDown){
+	public LiftOff2() {
+	}
+
+	public LiftOff2(int countDown) {
 		this.countDown = countDown;
 	}
 
-	public String status(){
-		return "#" + id + "(" + (countDown) +"), ";
+	public String status() {
+		return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!") + "), ";
 	}
 
 	@Override
 	public void run() {
-		while (countDown-- > 0){
-			if (Thread.currentThread().isInterrupted()){
-				System.out.println("Interrupted: #");
+		while (countDown-- > 0) {
+			if (Thread.currentThread().isInterrupted()) {
+				System.out.println("Interrupted: #" + id);
+				return;
 			}
+			System.out.println(status());
+			Thread.yield();
 		}
 	}
 }
@@ -38,5 +41,10 @@ class LiftOff2 implements Runnable {
 public class E20_InterruptCachedThreadPool {
 	public static void main(String[] args) {
 		ExecutorService exec = Executors.newCachedThreadPool();
+		for (int i = 0; i < 5; i++) {
+			exec.execute(new LiftOff2());
+		}
+		Thread.yield();
+		exec.shutdownNow();
 	}
 }
