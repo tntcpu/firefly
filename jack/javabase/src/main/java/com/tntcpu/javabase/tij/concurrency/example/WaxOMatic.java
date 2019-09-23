03539d8672b6572ab1d43907b062623937150096
zhangzhentao <tntcpu@126.com>
2019-09-23 18:01:50 +0800

    java concurrency add!

---
+++ jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/example/WaxOMatic.java
@@ -0,0 +1,10 @@
+package com.tntcpu.javabase.tij.concurrency.example;
+
+/**
+ * @program: firefly
+ * @description:
+ * @author: ZhangZhentao
+ * @create: 2019-09-23
+ **/
+public class WaxOMatic {
+}
