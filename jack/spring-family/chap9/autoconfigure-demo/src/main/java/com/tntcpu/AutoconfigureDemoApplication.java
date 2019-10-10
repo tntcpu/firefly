70dd6fc2485b448adee5541cb189f46cf29fda4c
tntcpu <gpscpu@gmail.com>
2019-10-11 07:16:13 +0800

    2019-09-29 chap9 autoconfigure-demo add!

---
+++ jack/spring-family/chap9/autoconfigure-demo/src/main/java/com/tntcpu/AutoconfigureDemoApplication.java
@@ -0,0 +1,17 @@
+package com.tntcpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AutoconfigureDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoconfigureDemoApplication.class, args);
+    }
+
+//    @Bean
+//    public GreetingApplicationRunner greetingApplicationRunner(){
+//        return new GreetingApplicationRunner("Spring");
+//    }
+}
---
+++ jack/spring-family/chap9/greeting/src/main/java/com/tntcpu/GreetingApplicationRunner.java
@@ -0,0 +1,17 @@
+package com.tntcpu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+@Slf4j
+public class GreetingApplicationRunner implements ApplicationRunner {
+    public GreetingApplicationRunner() {
+        log.info("Initializing GreetingApplicationRunner.");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Hello everyone!We all like Spring!");
+    }
+}
