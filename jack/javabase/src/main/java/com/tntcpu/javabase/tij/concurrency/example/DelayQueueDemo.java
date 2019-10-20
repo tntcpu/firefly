057c58acfdeb5dc08213b6588ba378dad8d5e244
tntcpu <gpscpu@gmail.com>
2019-10-20 22:55:41 +0800

    java concurrency add!

---
+++ jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/example/DelayQueueDemo.java
@@ -0,0 +1,131 @@
+package com.tntcpu.javabase.tij.concurrency.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+class DelayedTask implements Runnable, Delayed {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final int delta;
+    private final long trigger;
+    protected static List<DelayedTask> sequence = new ArrayList<>();
+
+    public DelayedTask(int delayInMilliseconds) {
+        delta = delayInMilliseconds;
+        trigger = System.nanoTime() + NANOSECONDS.convert(delta, MILLISECONDS);
+        sequence.add(this);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        DelayedTask that = (DelayedTask) o;
+        if (trigger < that.trigger) {
+            return -1;
+        }
+        if (trigger > that.trigger) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this + " ");
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(trigger - System.nanoTime(), NANOSECONDS);
+    }
+
+    public String toString() {
+        return String.format("[%1$-4d]", delta) + " task " + id;
+    }
+
+    public String summary() {
+        return "(" + id + ":" + delta + ")";
+    }
+
+    public static class EndSentinel extends DelayedTask {
+        private ExecutorService exec;
+
+        public EndSentinel(int delayInMilliseconds, ExecutorService e) {
+            super(delayInMilliseconds);
+            exec = e;
+        }
+
+        public void run() {
+            for (DelayedTask pt : sequence) {
+                System.out.println(pt.summary() + " ");
+            }
+            System.out.println();
+            System.out.println(this + " calling shutdownnow()");
+            exec.shutdownNow();
+        }
+    }
+}
+
+class DelayedTaskConsumer implements Runnable {
+    private DelayQueue<DelayedTask> q;
+
+    public DelayedTaskConsumer(DelayQueue<DelayedTask> q) {
+        this.q = q;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                q.take().run();
+            }
+        } catch (Exception e) {
+
+        }
+        System.out.println("finished delayedTaskConsumer");
+    }
+}
+
+public class DelayQueueDemo {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        DelayQueue<DelayedTask> queue = new DelayQueue<>();
+        for (int i = 0; i < 20; i++) {
+            queue.put(new DelayedTask(rand.nextInt(5000)));
+        }
+        queue.add(new DelayedTask.EndSentinel(5000, exec));
+        exec.execute(new DelayedTaskConsumer(queue));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
