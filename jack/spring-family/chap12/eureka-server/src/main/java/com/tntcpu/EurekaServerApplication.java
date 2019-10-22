8063b775d2dbdc39625d8006c9241bae1afdc375
tntcpu <gpscpu@gmail.com>
2019-10-22 23:15:50 +0800

    chap12 eureka-server&eureka-waiter-service add!

--- jack/spring-family/chap12/eureka-server/src/main/java/com/tntcpu/EurekaServerApplication.java
+++ jack/spring-family/chap12/eureka-server/src/main/java/com/tntcpu/EurekaServerApplication.java
@@ -1,13 +1,15 @@
-package com.tntcpu.eureka;
+package com.tntcpu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EnableEurekaServer.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
