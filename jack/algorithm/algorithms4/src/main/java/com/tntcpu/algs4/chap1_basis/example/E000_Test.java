b088d021224949597c083617702b89da4c40a210
tntcpu <tntcpu@126.com>
2019-11-04 23:31:04 +0800

    algorithms4 chap1 E000 add

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/example/E000_Test.java
@@ -0,0 +1,14 @@
+package com.tntcpu.algs4.chap1_basis.example;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2019-11-04
+ */
+public class E000_Test {
+    public static void main(String[] args) {
+        int c = 5/2;
+        System.out.println(c);
+    }
+}
