6366b8e9dcece885550f560e1ca7d75158e95df0
tntcpu <tntcpu@126.com>
2019-11-09 00:07:54 +0800

    algorithms4 chap1 Cat add.

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/utils/Cat.java
@@ -0,0 +1,20 @@
+package com.tntcpu.algs4.chap1_basis.utils;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2019-11-09
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Out out = new Out(args[args.length - 1]);
+        for (int i = 0; i < args.length - 1; i++) {
+            In in = new In(args[i]);
+            String s = in.readAll();
+            out.println(s);
+            in.close();
+        }
+        out.close();
+    }
+}
