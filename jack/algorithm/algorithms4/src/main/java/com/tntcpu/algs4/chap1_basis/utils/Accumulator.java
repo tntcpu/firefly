22584d042a50d460960b2c58ece9495ddeedbd8f
tntcpu <tntcpu@126.com>
2019-11-09 20:00:59 +0800

    algorithms4 chap1 Accumulator add.

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/utils/Accumulator.java
@@ -0,0 +1,26 @@
+package com.tntcpu.algs4.chap1_basis.utils;
+
+/**
+ * @program: firefly
+ * @desc: 累加器
+ * @author: tntcpu
+ * @create: 2019-11-09
+ */
+public class Accumulator {
+    private double total;
+    private int N;
+
+    public Accumulator(double val) {
+        N++;
+        total += val;
+    }
+
+    public double mean() {
+        return total / N;
+    }
+
+    public String toString() {
+        return "Mean (" + N + " values): "
+                + String.format("%7.5f", mean());
+    }
+}
