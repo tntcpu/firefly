a0c2e9b60d8285626165d436fa4d491f2a179302
tntcpu <tntcpu@126.com>
2019-11-09 19:16:29 +0800

    algorithms4 chap1 Flips add.

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/example/Flips.java
@@ -0,0 +1,30 @@
+package com.tntcpu.algs4.chap1_basis.example;
+
+import com.tntcpu.algs4.chap1_basis.utils.Counter;
+import com.tntcpu.algs4.chap1_basis.utils.StdOut;
+import com.tntcpu.algs4.chap1_basis.utils.StdRandom;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2019-11-09
+ */
+public class Flips {
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+        for (int i = 0; i < T; i++) {
+            if (StdRandom.bernoulli(0.5)) {
+                heads.increment();
+            } else {
+                tails.increment();
+            }
+        }
+        StdOut.println(heads);
+        StdOut.println(tails);
+        int i = heads.tally() - tails.tally();
+        StdOut.println("delta: " + Math.abs(i));
+    }
+}
