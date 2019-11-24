e8964bb945ad1f5f353efd08ef9ecf16948aad6f
tntcpu <tntcpu@126.com>
2019-11-24 20:32:08 +0800

    algorithms4 chap1 E007 add.

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/example/E007_Evaluate.java
@@ -0,0 +1,71 @@
+package com.tntcpu.algs4.chap1_basis.example;
+
+import com.tntcpu.algs4.chap1_basis.utils.StdIn;
+import com.tntcpu.algs4.chap1_basis.utils.StdOut;
+
+import java.util.Stack;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2019-11-24
+ */
+public class E007_Evaluate {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("(")) {
+            } else if (s.equals("+")) {
+                ops.push(s);
+            } else if (s.equals("-")) {
+                ops.push(s);
+            } else if (s.equals("*")) {
+                ops.push(s);
+            } else if (s.equals("/")) {
+                ops.push(s);
+            } else if (s.equals("sqrt")) {
+                ops.push(s);
+            } else if (s.equals(")")) {
+                String op = ops.pop();
+                Double v = vals.pop();
+                if (op.equals("+")) {
+                    v = vals.pop() + v;
+                } else if (op.equals("-")) {
+                    v = vals.pop() - v;
+                } else if (op.equals("sqrt")) {
+                    v = Math.sqrt(v);
+                } else {
+                    vals.push(Double.parseDouble(s));
+                }
+                StdOut.println(vals.pop());
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
