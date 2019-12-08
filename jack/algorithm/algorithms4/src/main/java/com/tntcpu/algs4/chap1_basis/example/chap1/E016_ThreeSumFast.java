a9c4c88db788851cc1fa9226931afc682785b458
tntcpu <tntcpu@126.com>
2019-12-08 23:46:37 +0800

    algorithms4 chap1 E016 add.

---
+++ jack/algorithm/algorithms4/src/main/java/com/tntcpu/algs4/chap1_basis/example/chap1/E016_ThreeSumFast.java
@@ -0,0 +1,42 @@
+package com.tntcpu.algs4.chap1_basis.example.chap1;
+
+import com.tntcpu.algs4.chap1_basis.utils.StdOut;
+import com.tntcpu.algs4.chap1_basis.utils.StdRandom;
+import com.tntcpu.algs4.chap1_basis.utils.StopWatch;
+
+import java.util.Arrays;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2019-12-08
+ */
+public class E016_ThreeSumFast {
+    public static int count(int[] a) {
+        Arrays.sort(a);
+        int n = a.length;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (E002_BinarySearch.rank(-a[i] - a[j], a) > j) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int n = 30000;
+        int max = 1000000;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = StdRandom.uniform(-max, max);
+        }
+        StopWatch timer = new StopWatch();
+        StdOut.println(count(a));
+        double time = timer.elapsedTime();
+        System.out.println("time: " + time + "s");
+    }
+}
