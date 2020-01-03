541303aed4a48f3538869a9c668a533f0a9c03f7
tntcpu <tntcpu@126.com>
2020-01-03 13:45:55 +0800

    agile Game2 add!

--- jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game2.java
+++ jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game2.java
@@ -6,25 +6,25 @@ package com.tntcpu.agile.chap6;
  * @author: ZhangZhentao
  * @create: 2020-01-03
  **/
-public class Game1 {
+public class Game2 {
 	private int itsScore = 0;
 	private int[] itsThrows = new int[21];
 	private int itsCurrentThrow = 0;
 
-	public int score() {
+	public int score(){
 		return itsScore;
 	}
-
-	public void add(int pins) {
+	public void add(int pins){
 		itsThrows[itsCurrentThrow++] = pins;
 		itsScore += pins;
 	}
 
-	public int scoreForFrame(int theFrame) {
-		int ball = 0;
+	public int scoreForFrame(int frame){
 		int score = 0;
-		for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-			score += itsThrows[ball++] + itsThrows[ball++];
+		for (int ball = 0;
+		      frame > 0 && (ball < itsCurrentThrow);
+		      ball += 2, frame--){
+			score += itsThrows[ball] + itsThrows[ball+1];
 		}
 		return score;
 	}
