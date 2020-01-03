4938d6c185703eaca9c4974bf1aeeb5db291f5f9
tntcpu <tntcpu@126.com>
2020-01-03 16:18:22 +0800

    agile Game7 update!

---
+++ jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game7.java
@@ -0,0 +1,51 @@
+package com.tntcpu.agile.chap6;
+
+/**
+ * @program: firefly
+ * @description:
+ * @author: ZhangZhentao
+ * @create: 2020-01-03
+ **/
+public class Game7 {
+	private int itsScore = 0;
+	private int[] itsThrows = new int[21];
+	private int itsCurrentThrow = 0;
+	private int itsCurrentFrame = 0;
+	private boolean firstThrow = true;
+
+	public int getCurrentFrame() {
+		return itsCurrentFrame;
+	}
+
+	public void add(int pins) {
+		itsThrows[itsCurrentThrow++] = pins;
+		itsScore += pins;
+		adjustCurrentFrame();
+	}
+
+	private void adjustCurrentFrame() {
+		if (firstThrow) {
+			firstThrow = false;
+			itsCurrentFrame++;
+		} else {
+			firstThrow = true;
+		}
+	}
+
+	public int scoreForFrame(int theFrame) {
+		int ball = 0;
+		int score = 0;
+		for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+			int firstThrow = itsThrows[ball++];
+			int secondFrame = itsThrows[ball++];
+
+			int frameScore = firstThrow + secondFrame;
+			if (frameScore == 10) {
+				score += frameScore + itsThrows[ball];
+			} else {
+				score += frameScore;
+			}
+		}
+		return score;
+	}
+}
