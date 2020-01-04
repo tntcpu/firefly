16858d95c1d9c0e52c58002a680f2b18167adbc1
tntcpu <tntcpu@126.com>
2020-01-04 13:58:47 +0800

    agile Game12 add!

---
+++ jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game12.java
@@ -0,0 +1,66 @@
+package com.tntcpu.agile.chap6;
+
+/**
+ * @program: firefly
+ * @description:
+ * @author: ZhangZhentao
+ * @create: 2020-01-03
+ **/
+public class Game12 {
+    private int ball;
+    private int firstThrow;
+    private int secondThrow;
+
+    private int itsScore = 0;
+    private int[] itsThrows = new int[21];
+    private int itsCurrentThrow = 0;
+    private int itsCurrentFrame = 1;
+    private boolean firstThrowInFrame = true;
+
+    public int score() {
+        return scoreForFrame(itsCurrentFrame);
+    }
+
+    public int getCurrentFrame() {
+        return itsCurrentFrame;
+    }
+
+    public void add(int pins) {
+        itsThrows[itsCurrentThrow++] = pins;
+        itsScore += pins;
+        adjustCurrentFrame(pins);
+    }
+
+    private void adjustCurrentFrame(int pins) {
+
+    }
+
+    public int scoreForFrame(int theFrame) {
+        ball = 0;
+        int score = 0;
+        for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+            firstThrow = itsThrows[ball];
+            if (firstThrow == 10){
+                ball++;
+                score += 10 + itsThrows[ball] + itsThrows[ball+1];
+            }else {
+                score += handleSecondThrow();
+            }
+        }
+        return score;
+    }
+
+    private int handleSecondThrow() {
+        int score = 0;
+        secondThrow = itsThrows[ball+1];
+        int frameScore = firstThrow + secondThrow;
+        if (frameScore == 10) {
+            ball += 2;
+            score += frameScore + itsThrows[ball];
+        } else {
+            ball += 2;
+            score += frameScore;
+        }
+        return score;
+    }
+}
