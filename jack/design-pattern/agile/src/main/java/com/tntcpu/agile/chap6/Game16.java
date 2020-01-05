98cb8ebcae0df2c0568264adca9d65eeb1ed091a
tntcpu <tntcpu@126.com>
2020-01-05 09:24:49 +0800

    agile Game16 update!

--- jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game16.java
+++ jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game16.java
@@ -27,15 +27,19 @@ public class Game16 {
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame) {
             if (pins == 10) {
-                itsCurrentFrame++;
+                advanceFrame();
             } else {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
-            itsCurrentFrame++;
+            advanceFrame();
         }
         itsCurrentFrame = Math.min(11, itsCurrentFrame);
+    }
+
+    private void advanceFrame() {
+        itsCurrentFrame = Math.min(11, itsCurrentFrame + 1);
     }
 
     public int scoreForFrame(int theFrame) {
