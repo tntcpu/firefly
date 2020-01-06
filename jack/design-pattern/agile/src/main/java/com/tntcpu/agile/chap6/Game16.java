4ce2c10d3c14509ca6e9553edd2e47ecd7ad961b
tntcpu <tntcpu@126.com>
2020-01-06 10:32:48 +0800

    agile Game16 update!

--- jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game16.java
+++ jack/design-pattern/agile/src/main/java/com/tntcpu/agile/chap6/Game16.java
@@ -7,40 +7,49 @@ package com.tntcpu.agile.chap6;
  * @create: 2020-01-03
  **/
 public class Game16 {
-    private int itsScore = 0;
-    private int itsCurrentFrame = 0;
-    private boolean firstThrowInFrame = true;
-    private Scorer itsScorer = new Scorer();
+	private int itsScore = 0;
+	private int itsCurrentFrame = 0;
+	private boolean firstThrowInFrame = true;
+	private Scorer itsScorer = new Scorer();
 
-    public int score() {
-        return scoreForFrame(getCurrentFrame() - 1);
-    }
+	public int score() {
+		return scoreForFrame(getCurrentFrame());
+	}
 
-    public int getCurrentFrame() {
-        return itsCurrentFrame;
-    }
+	public int getCurrentFrame() {
+		return itsCurrentFrame;
+	}
 
-    public void add(int pins) {
-        itsScorer.addThrow(pins);
-        itsScore += pins;
-        adjustCurrentFrame(pins);
-    }
+	public void add(int pins) {
+		itsScorer.addThrow(pins);
+		itsScore += pins;
+		adjustCurrentFrame(pins);
+	}
 
-    private void adjustCurrentFrame(int pins) {
-        if (firstThrowInFrame) {
-            if (pins == 10) {
-                itsCurrentFrame++;
-            } else {
-                firstThrowInFrame = false;
-            }
-        } else {
-            firstThrowInFrame = true;
-            itsCurrentFrame++;
-        }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
-    }
+	private void adjustCurrentFrame(int pins) {
+		if (firstThrowInFrame) {
+			if (!adjustFrameForStrike(pins)) {
+				firstThrowInFrame = false;
+			}
+		} else {
+			firstThrowInFrame = true;
+			advanceFrame();
+		}
+	}
 
-    public int scoreForFrame(int theFrame) {
-        return itsScorer.scoreForFrame(theFrame);
-    }
+	private boolean adjustFrameForStrike(int pins) {
+		if (pins == 10) {
+			advanceFrame();
+			return true;
+		}
+		return false;
+	}
+
+	private void advanceFrame() {
+		itsCurrentFrame = Math.min(10, itsCurrentFrame + 1);
+	}
+
+	public int scoreForFrame(int theFrame) {
+		return itsScorer.scoreForFrame(theFrame);
+	}
 }
