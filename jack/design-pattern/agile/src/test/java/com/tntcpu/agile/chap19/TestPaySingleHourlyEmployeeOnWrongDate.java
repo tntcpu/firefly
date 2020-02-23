d6b321227c329775487103d06e5caedc089b7fd9
tntcpu <tntcpu@126.com>
2020-02-23 19:44:03 +0800

    agile payroll TestPaySingleHourlyEmployeeOnWrongDate commit!

---
+++ jack/design-pattern/agile/src/test/java/com/tntcpu/agile/chap19/TestPaySingleHourlyEmployeeOnWrongDate.java
@@ -0,0 +1,41 @@
+package com.tntcpu.agile.chap19;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * @program: firefly
+ * @desc:
+ * @author: tntcpu
+ * @create: 2020-02-23
+ */
+public class TestPaySingleHourlyEmployeeOnWrongDate {
+    private PayrollDatabase database;
+
+    @Before
+    public void before() {
+        database = new InMemoryPayrollDatabase();
+    }
+
+    @Test
+    public void testPaySingleHourlyEmployeeOnWrongDate() throws Exception {
+        int empId = 2;
+        AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bill", "Home", 15.25, database);
+        t.execute();
+        Calendar cal = Calendar.getInstance();
+        cal.set(2020, 1, 20);
+        Date payDate = cal.getTime();
+        TimeCardTransaction tc = new TimeCardTransaction(payDate, 9.0, empId, database);
+        tc.execute();
+        PaydayTransaction pt = new PaydayTransaction(payDate, database);
+        pt.execute();
+        Paycheck pc = pt.getPaycheck(empId);
+        assertNull(pc);
+
+    }
+}
