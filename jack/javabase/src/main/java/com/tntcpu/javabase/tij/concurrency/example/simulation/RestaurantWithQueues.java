905c4919e5b19882d197926ba258ce2929454742
tntcpu <tntcpu@126.com>
2020-03-11 18:26:59 +0800

    tij concurrency RestaurantWithQueues add!

---
+++ jack/javabase/src/main/java/com/tntcpu/javabase/tij/concurrency/example/simulation/RestaurantWithQueues.java
@@ -0,0 +1,14 @@
+package com.tntcpu.javabase.tij.concurrency.example.simulation;
+
+/**
+ * @program: firefly
+ * @description:
+ * @author: ZhangZhentao
+ * @create: 2020-03-11
+ **/
+class Order {
+
+}
+
+public class RestaurantWithQueues {
+}
